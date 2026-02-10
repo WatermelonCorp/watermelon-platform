a260f9ec3d560c3c2d1d57be3da1e151ab82533c
Muhammed Sanjid <sanjid357@gmail.com>
2026-02-10 14:29:19 +0530

    feat: removed demo components

--- src/data/contents/components/animated-button/demo.tsx
+++
@@ -1,9 +0,0 @@
-import { AnimatedButton } from ".";
-
-export default function AnimatedButtonDemo() {
-  return (
-    <div className="flex items-center justify-center p-10">
-      <AnimatedButton>Click Me</AnimatedButton>
-    </div>
-  );
-}
