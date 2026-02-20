5ff35262af8b8ce3886c33972cb681f29dfaff9c
wtfdivyansh <divyanshgupta19305@gmail.com>
2026-02-20 22:17:26 +0530

    fix:fluid-tabs

--- src/data/contents/components/fluid-tabs/demo.tsx
+++ src/data/contents/components/fluid-tabs/demo.tsx
@@ -1,20 +1,17 @@
-import { FluidTabs } from ".";
-import { BiSolidPieChartAlt2 } from "react-icons/bi";
-import { FaInbox, FaLandmark } from "react-icons/fa";
+import { FluidTabs } from '.';
+import { BiSolidPieChartAlt2 } from 'react-icons/bi';
+import { FaInbox, FaLandmark } from 'react-icons/fa';
 
 const tabs = [
-    { id: "accounts", label: "Accounts", icon: <FaLandmark size={22} /> },
-    { id: "deposits", label: "Deposits", icon: <FaInbox size={22} /> },
-    { id: "funds", label: "Funds", icon: <BiSolidPieChartAlt2 size={22} /> },
+  { id: 'accounts', label: 'Accounts', icon: <FaLandmark size={22} /> },
+  { id: 'deposits', label: 'Deposits', icon: <FaInbox size={22} /> },
+  { id: 'funds', label: 'Funds', icon: <BiSolidPieChartAlt2 size={22} /> },
 ];
 
 export default function FluidTabsDemo() {
-    return (
-        <div className="flex justify-center items-center">
-            <FluidTabs
-                defaultActive="home"
-                tabs={tabs}
-            />
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center">
+      <FluidTabs tabs={tabs} />
+    </div>
+  );
 }
