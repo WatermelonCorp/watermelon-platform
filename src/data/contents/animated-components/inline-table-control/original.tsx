4d0c7e2304ef99a6b0cc9222263001755129b54c
wtfdivyansh <divyanshgupta19305@gmail.com>
2026-05-15 20:56:11 +0530

    fix lint issues

--- src/data/contents/animated-components/inline-table-control/original.tsx
+++ src/data/contents/animated-components/inline-table-control/original.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion, AnimatePresence, LayoutGroup } from 'motion/react';
 import { Pencil, X, Check } from 'lucide-react';
 import { GoStack } from 'react-icons/go';
