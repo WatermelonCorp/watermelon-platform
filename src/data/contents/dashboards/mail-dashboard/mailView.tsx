b037ad189550b779ac627d91dc05f6a8a686b09c
wtfdivyansh <divyanshgupta19305@gmail.com>
2026-06-14 21:12:40 +0530

    add dashboard

--- src/data/contents/dashboards/mail-dashboard/mailView.tsx
+++
@@ -1,7 +0,0 @@
-import { MailViewer } from "./components/mail/mail-viewer"
-
-export default async function Page({ params }: { params: { id: string } }) {
-
-   const { id } = await params
-   return <MailViewer id={id} />
-}
